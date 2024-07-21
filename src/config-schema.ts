d408350e5660e3e4f6c6c3ed207092b641398407
vijay <vijaykv2228@gmail.com>
2024-07-21 21:08:45 +0530

    feat : added tutorials for patient registration

--- src/config-schema.ts
+++ src/config-schema.ts
@@ -1,4 +1,4 @@
-import {Type} from '@openmrs/esm-framework';
+import { Type } from '@openmrs/esm-framework';
 
 export const configSchema = {
   showTutorial: {
@@ -13,6 +13,7 @@ export const configSchema = {
       {
         title: 'Basic Tutorial',
         description: 'Learn how to efficiently search for patients, register new patients, access user settings, and view ongoing visits and appointments.',
+        link : 'home',
         steps: [{
           target: '[aria-label="OpenMRS"]',
           content: 'Welcome to OpenMRS! This is the main dashboard where you can navigate to various features of the system.'
@@ -42,17 +43,50 @@ export const configSchema = {
       {
         title: 'Option Tutorial',
         description: 'Optional Tutorial for debugging purposes',
+        link: 'home',
         steps: [{
           target: '[aria-label="OpenMRS"]',
           content: 'Welcome to OpenMRS! This is the main dashboard where you can navigate to various features of the system.'
         },
           {
             target: '[name="SearchPatientIcon"]',
-            content: 'This is the search icon. Use it to find patients in the system quickly.'
-          }]
-      }
-    ]
-  }
+            content: 'This is the search icon. Use it to find patients in the system quickly.',
+          },
+        ],
+      },
+      {
+        title: 'Patient Registration Tutorial',
+        description: 'Learn how to register a new patient into the system.',
+        link: 'patient-registration',
+        steps: [
+          {
+            target: '[name="AddPatientIcon"]',
+            title: 'Add Patient',
+            content: 'Click here to add a patient to the system.',
+            disableBeacon: true,
+          },
+          {
+            target: '[aria-label="Demographics Section"]',
+            title: 'Demographics',
+            content: 'This is the Demographics section. Here you can find various fields and information related to the patient.',
+            disableBeacon: true,
+          },
+          {
+            target: '[aria-label="Contact Details Section"]',
+            title: 'Contact Details',
+            content: "Here you can update the patient's contact information.",
+            disableBeacon: true,
+          },
+          {
+            target: '[aria-label="Relationships section"]',
+            title: 'Relationships',
+            content: "In this section, you can manage the patient's relationships.",
+            disableBeacon: true,
+          },
+        ],
+      },
+    ],
+  },
 };
 
 export type Config = {
@@ -60,8 +94,10 @@ export type Config = {
   tutorialData: {
     title: string;
     description: string;
+    link: string;
     steps: {
       target: string;
+      title: string;
       content: string;
       disableBeacon: boolean;
     }[];
